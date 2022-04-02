4773cf229924b24f071567834efa949d50a811b9
BENJIGAO <bhgao20014@gmail.com>
2022-04-02 17:40:39 -0400

    refactor(code): move background to base component

--- src/features/Dashboard/index.tsx
+++ src/features/Dashboard/index.tsx
@@ -7,6 +7,7 @@ import Channels from "./Channels";
 import LikedVideos from "./LikedVideos";
 import Statistics from "./Statistics";
 import Subscriptions from "./Subscriptions";
+import Toolbar from "@mui/material/Toolbar";
 
 const drawerWidth = 250;
 
@@ -15,15 +16,29 @@ const Dashboard = () => {
     <Box sx={{ display: "flex" }}>
       <AppBar drawerWidth={drawerWidth} />
       <Drawer drawerWidth={drawerWidth} />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/subscriptions" element={<Subscriptions />} />
-        <Route path="/liked-videos" element={<LikedVideos />} />
-        <Route path="/channels" element={<Channels />} />
-        <Route path="/statistics" element={<Statistics />} />
-        <Route path="/" element={<Navigate to="/dashboard/home" />} />
-        <Route path="*" element={<div>Not found</div>} />
-      </Routes>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <Toolbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/subscriptions" element={<Subscriptions />} />
+          <Route path="/liked-videos" element={<LikedVideos />} />
+          <Route path="/channels" element={<Channels />} />
+          <Route path="/statistics" element={<Statistics />} />
+          <Route path="/" element={<Navigate to="/dashboard/home" />} />
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+      </Box>
     </Box>
   );
 };
