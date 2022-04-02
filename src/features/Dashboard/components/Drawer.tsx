6c92dd5afc5d9d3100aab5769a91200a3569b778
BENJIGAO <bhgao20014@gmail.com>
2022-04-02 17:09:22 -0400

    feat(routing): add drawer items

--- src/features/Dashboard/components/Drawer.tsx
+++ src/features/Dashboard/components/Drawer.tsx
@@ -5,14 +5,17 @@ import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import ListItemLink from "features/Dashboard/components/ListItemLink";
 import HomeIcon from "@mui/icons-material/Home";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InsightsIcon from "@mui/icons-material/Insights";
+import BubbleChartIcon from "@mui/icons-material/BubbleChart";
 
 interface IDrawerProps {
   drawerWidth: number;
 }
 
-const CustomDrawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
+const CustomDrawer = styled(MuiDrawer)(() => ({
   "& .MuiDrawer-paper": {
     position: "relative",
     whiteSpace: "nowrap",
@@ -28,13 +31,35 @@ const Drawer = (props: IDrawerProps) => {
         sx={{
           display: "flex",
           alignItems: "center",
-          justifyContent: "flex-end",
+          justifyContent: "flex-start",
           px: 1,
         }}
-      ></Toolbar>
+      >
+        Youtistics
+      </Toolbar>
       <Divider />
       <List component="nav">
         <ListItemLink to="/dashboard/home" primary="Home" icon={<HomeIcon />} />
+        <ListItemLink
+          to="/dashboard/subscriptions"
+          primary="Subscriptions"
+          icon={<SubscriptionsIcon />}
+        />
+        <ListItemLink
+          to="/dashboard/liked-videos"
+          primary="Liked Videos"
+          icon={<ThumbUpIcon />}
+        />
+        <ListItemLink
+          to="/dashboard/channels"
+          primary="Other Channels"
+          icon={<YouTubeIcon />}
+        />
+        <ListItemLink
+          to="/dashboard/statistics"
+          primary="Fun Statistics"
+          icon={<InsightsIcon />}
+        />
       </List>
     </CustomDrawer>
   );
