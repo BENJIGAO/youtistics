5e6150ff49a5be24434d00751c3ef30944d77670
BENJIGAO <bhgao20014@gmail.com>
2022-04-03 17:16:38 -0400

    feat(menu): add account menu on dashboard

--- src/features/Dashboard/components/AppBar.tsx
+++ src/features/Dashboard/components/AppBar.tsx
@@ -1,14 +1,36 @@
-import { Avatar } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Avatar,
+  Tooltip,
+  IconButton,
+  Menu,
+  MenuItem,
+  Divider,
+  ListItemIcon,
+} from "@mui/material";
 import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Box } from "@mui/system";
+import SwitchAccountIcon from "@mui/icons-material/SwitchAccount";
+import Logout from "@mui/icons-material/Logout";
 
 interface IAppBarProps {
   drawerWidth: number;
 }
 
 const AppBar = ({ drawerWidth }: IAppBarProps) => {
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <MuiAppBar
       position="absolute"
@@ -24,7 +46,71 @@ const AppBar = ({ drawerWidth }: IAppBarProps) => {
         >
           Dashboard
         </Typography>
-        <Avatar>B</Avatar>
+        <Tooltip title="Google Account">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32 }}>B</Avatar>
+          </IconButton>
+        </Tooltip>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&:before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <MenuItem onClick={() => console.log("TODO")}>
+            <Avatar />
+            Profile
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={() => navigate("/home")}>
+            <ListItemIcon>
+              <SwitchAccountIcon fontSize="small" />
+            </ListItemIcon>
+            Switch Accounts
+          </MenuItem>
+          <MenuItem onClick={() => navigate("/home")}>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            Logout
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </MuiAppBar>
   );
