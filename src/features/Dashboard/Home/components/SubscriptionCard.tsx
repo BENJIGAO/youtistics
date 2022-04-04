d6682cc8bb8bb508e4087b11c1cfe3714184d9d7
BENJIGAO <bhgao20014@gmail.com>
2022-04-03 21:47:02 -0400

    feat(home): add tooltip to subscription card

--- src/features/Dashboard/Home/components/SubscriptionCard.tsx
+++ src/features/Dashboard/Home/components/SubscriptionCard.tsx
@@ -1,10 +1,15 @@
-import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { Tooltip } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const CustomCard = styled(Card)(({ theme }) => ({
+  "&:hover": { boxShadow: theme.shadows[12] },
+}));
 
 interface ISubscriptionCard {
   title?: string;
@@ -18,23 +23,24 @@ const SubscriptionCard = ({
   imageUrl,
 }: ISubscriptionCard) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ maxHeight: 200 }}
-        component="img"
-        image={imageUrl}
-        alt={title}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Tooltip title={title ?? "Loading..."} placement="bottom">
+      <CustomCard onClick={() => console.log("hello")}>
+        <CardMedia
+          sx={{ maxHeight: 200 }}
+          component="img"
+          image={imageUrl}
+          alt={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </CustomCard>
+    </Tooltip>
   );
 };
 
