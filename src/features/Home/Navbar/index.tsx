dee3516e9dc7dda5335b42d84374bb09ccff0641
BENJIGAO <bhgao20014@gmail.com>
2022-04-10 13:16:54 -0400

    cleanup(code): remove tailwindcss from project

--- src/features/Home/Navbar/index.tsx
+++ src/features/Home/Navbar/index.tsx
@@ -1,21 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
 
-interface IHomeNavbarProps {
-}
+interface IHomeNavbarProps {}
 
 const HomeNavbar: React.FC<IHomeNavbarProps> = (props) => {
   return (
-    <div className='flex justify-center w-full'>
-      <div className='flex justify-between w-3/5'>
-        <div>
-          Youtistics
-        </div>
-        <div>
-          Get Started
-        </div>
-        <div>
-          Developer
-        </div>
+    <div>
+      <div>
+        <div>Youtistics</div>
+        <div>Get Started</div>
+        <div>Developer</div>
       </div>
     </div>
   );
--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import HomeNavbar from 'features/Home/Navbar'
-import AuthButton from 'features/Home/AuthButton'
+import React from "react";
+import { Link } from "react-router-dom";
+import HomeNavbar from "features/Home/Navbar";
+import AuthButton from "features/Home/AuthButton";
 
 const Home: React.FC = () => {
   return (
     <div>
       <HomeNavbar />
-      <h1 className='text-2xl'>Home</h1>
+      <h1>Home</h1>
       <nav>
-        <Link to='/dashboard'>Dashboard</Link>
+        <Link to="/dashboard">Dashboard</Link>
       </nav>
       <AuthButton />
     </div>
   );
-}
+};
 
 export default Home;
