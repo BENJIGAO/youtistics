97b709f38271f1b174714f55c2fc1d9858f4419d
BENJIGAO <bhgao20014@gmail.com>
2022-04-10 13:06:31 -0400

    cleanup(code): remove index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CssBaseline from '@mui/material/CssBaseline'
-import { createTheme } from '@mui/material/styles';
-import App from 'app/app';
-import './index.css';
-import { ThemeProvider } from '@emotion/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme } from "@mui/material/styles";
+import App from "app/app";
+import { ThemeProvider } from "@emotion/react";
 
-const theme = createTheme()
+const theme = createTheme();
 
 ReactDOM.render(
-    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
