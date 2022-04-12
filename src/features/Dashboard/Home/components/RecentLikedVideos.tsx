f3c3705e912ad76e8b205637abe552448e11eb8e
BENJIGAO <bhgao20014@gmail.com>
2022-04-11 20:28:54 -0400

    refactor(home): move subscriptions and liked videos into it's own component in home page

---
+++ src/features/Dashboard/Home/components/RecentLikedVideos.tsx
@@ -0,0 +1,7 @@
+import Grid from "@mui/material/Grid";
+
+const RecentLikedVideos = () => {
+  return <Grid item xs={12}></Grid>;
+};
+
+export default RecentLikedVideos;
--- src/features/Dashboard/Home/index.tsx
+++ src/features/Dashboard/Home/index.tsx
@@ -1,74 +1,11 @@
-import { useEffect, useState } from "react";
-import { Navigation } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
-import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
-import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { Subscription, ChannelStatistics, Channel } from "@types";
 import Copyright from "common/components/Copyright";
-import { getSubscriptions, getChannelByIds } from "common/utils/apiUtils";
-import SubscriptionCard from "features/Dashboard/Home/components/SubscriptionCard";
-
-import "swiper/css";
-import "swiper/css/navigation";
+import RecentSubscriptions from "features/Dashboard/Home/components/RecentSubscriptions";
+import RecentLikedVideos from "features/Dashboard/Home/components/RecentLikedVideos";
 
 const Home = () => {
-  const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
-  const [channelStats, setChannelStats] = useState<ChannelStatistics[]>([]);
-
-  // Gets users subscriptions on load
-  useEffect(() => {
-    getSubscriptions(15).then((subscriptions) =>
-      setSubscriptions(subscriptions ?? [])
-    );
-  }, []);
-
-  // Set channelStats when subscriptions is populated because it needs it for sorting as a reference array
-  useEffect(() => {
-    setChannelStatsWithSubscriptions(subscriptions);
-  }, [subscriptions]);
-
-  const setChannelStatsWithSubscriptions = (
-    subs: Subscription[] | undefined
-  ): void => {
-    if (subs === undefined) {
-      return;
-    }
-    const subscriptionIds = subs
-      .map((sub) => sub.snippet?.resourceId?.channelId)
-      .filter((id): id is string => id !== undefined);
-
-    getChannelByIds(subscriptionIds).then((channels) =>
-      sortAndSetChannelStats(channels)
-    );
-  };
-
-  const sortAndSetChannelStats = (channels: Channel[] | undefined) => {
-    if (channels === undefined) {
-      return;
-    }
-    const refArr = subscriptions
-      .map((channel) => channel.snippet?.title)
-      .filter((title): title is string => title !== undefined);
-
-    const sortedChannels: Channel[] = channels.slice().sort((a, b) => {
-      return (
-        refArr.indexOf(a.snippet?.localized?.title || "") -
-        refArr.indexOf(b.snippet?.localized?.title || "")
-      );
-    });
-
-    const sortedStatistics = sortedChannels
-      .map((channel) => channel.statistics)
-      .filter((stats): stats is ChannelStatistics => stats !== undefined);
-
-    setChannelStats(sortedStatistics);
-  };
-
   return (
     <Box sx={{ m: 4 }}>
       <Grid container rowSpacing={3}>
@@ -77,67 +14,13 @@ const Home = () => {
             Recent Subscriptions
           </Typography>
         </Grid>
-        <Grid item xs={12} sx={{ m: 0, pr: 5 }}>
-          {subscriptions !== [] ? (
-            <Swiper
-              style={{ position: "relative" }}
-              modules={[Navigation]}
-              spaceBetween={15}
-              breakpoints={{
-                0: { slidesPerView: 1 },
-                600: { slidesPerView: 2 },
-                900: { slidesPerView: 3 },
-                1200: { slidesPerView: 4 },
-                1536: { slidesPerView: 5 },
-              }}
-              simulateTouch={false}
-              navigation={{ nextEl: ".next", prevEl: ".prev" }}
-            >
-              {subscriptions.map((sub, index) => {
-                return (
-                  <SwiperSlide key={index}>
-                    <SubscriptionCard
-                      channelTitle={sub.snippet?.title}
-                      channelDescription={sub.snippet?.description}
-                      channelImageUrl={sub.snippet?.thumbnails?.high?.url}
-                      channelStats={channelStats[index]}
-                    />
-                  </SwiperSlide>
-                );
-              })}
-              <IconButton
-                sx={{
-                  position: "absolute",
-                  zIndex: 10,
-                  bottom: "45%",
-                  right: 0,
-                }}
-                className="next"
-              >
-                <NavigateNextIcon sx={{ fontSize: 50 }} color="primary" />
-              </IconButton>
-              <IconButton
-                sx={{
-                  position: "absolute",
-                  zIndex: 10,
-                  bottom: "45%",
-                  left: 0,
-                }}
-                className="prev"
-              >
-                <NavigateBeforeIcon sx={{ fontSize: 50 }} color="primary" />
-              </IconButton>
-            </Swiper>
-          ) : (
-            <Skeleton variant="rectangular" height={300} />
-          )}
-        </Grid>
+        <RecentSubscriptions />
         <Grid item xs={12}>
           <Typography component="h2" variant="h4">
             Recent Liked Videos
           </Typography>
         </Grid>
-        <Grid item xs={12}></Grid>
+        <RecentLikedVideos />
       </Grid>
       <Copyright sx={{ pt: 4 }} />
     </Box>
