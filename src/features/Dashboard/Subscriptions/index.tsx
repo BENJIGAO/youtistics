2a2d6a205ff1292ee7937cc470600af35f8b7f4c
BENJIGAO <bhgao20014@gmail.com>
2022-04-17 18:25:33 -0400

    feat(subs): figure out actual basic layout (ignore previous commit)

--- src/features/Dashboard/Subscriptions/index.tsx
+++ src/features/Dashboard/Subscriptions/index.tsx
@@ -6,45 +6,60 @@ import Typography from "@mui/material/Typography";
 import ScatterChart from "features/Dashboard/Subscriptions/components/charts/ScatterChart";
 import PieChart from "features/Dashboard/Subscriptions/components/charts/PieChart";
 import GaugeChart from "features/Dashboard/Subscriptions/components/charts/GaugeChart";
+import Copyright from "common/components/Copyright";
 
 const Subscriptions = () => {
   return (
     <Box sx={{ m: 4 }}>
       <Grid container spacing={2}>
         <Grid item xs={12} lg={6}>
-          <Paper sx={{ height: 480 }}>
+          <Paper sx={{ height: 520 }}>
             <PieChart />
           </Paper>
         </Grid>
         <Grid item xs={12} lg={6}>
           <Stack spacing={2}>
-            <Paper sx={{ height: 232 }}>
-              <ScatterChart />
-            </Paper>
-            <Paper sx={{ height: 232 }}>
+            <Stack direction="row" spacing={2} sx={{ height: 252 }}>
+              <Paper sx={{ width: "50%" }}>
+                <Typography variant="h6">
+                  Sports were your favourite category
+                </Typography>
+              </Paper>
+              <Paper sx={{ width: "50%" }}>
+                <Typography variant="h6">
+                  Cooking was your least favourite category
+                </Typography>
+              </Paper>
+            </Stack>
+            <Paper sx={{ height: 252 }}>
               <ScatterChart />
             </Paper>
           </Stack>
         </Grid>
         <Grid item xs={12} lg={6} xl={2}>
-          <Paper sx={{ height: 232 }}>
+          <Paper sx={{ height: 252 }}>
             <GaugeChart />
           </Paper>
         </Grid>
-        <Grid item xs={12} lg={6} xl={3}>
-          <Paper sx={{ height: 232 }}>
+        <Grid item xs={12} lg={6} xl={2}>
+          <Paper sx={{ height: 252 }}>
             <GaugeChart />
           </Paper>
         </Grid>
-        <Grid item xs={12} lg={6} xl={3}>
-          <Paper sx={{ height: 232 }}>
-            <GaugeChart />
+        <Grid item xs={12} lg={6} xl={6}>
+          <Paper sx={{ height: 252 }}>
+            <ScatterChart />
           </Paper>
         </Grid>
-        <Grid item xs={12} lg={6} xl={4}>
-          <Paper sx={{ height: 232 }}>
-            <GaugeChart />
+        <Grid item xs={12} lg={6} xl={2}>
+          <Paper sx={{ height: 252 }}>
+            <Typography variant="h6">
+              RDCWorld1 is your favourite channel!
+            </Typography>
           </Paper>
+        </Grid>
+        <Grid item xs={12} sx={{ mt: 5 }}>
+          <Copyright />
         </Grid>
       </Grid>
     </Box>
