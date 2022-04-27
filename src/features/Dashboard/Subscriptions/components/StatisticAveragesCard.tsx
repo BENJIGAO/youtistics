ad487a7ca869e124c44f6ec61494273b21d4d571
Brian Gao <bhgao20014@gmail.com>
2022-04-27 19:54:44 -0400

    refactor(subs): remove gauge chart and add statistics average card skeleton

---
+++ src/features/Dashboard/Subscriptions/components/StatisticAveragesCard.tsx
@@ -0,0 +1,9 @@
+import Paper from "@mui/material/Paper";
+
+interface IStatisticAveragesCardProps {}
+
+const StatisticAveragesCard = ({}: IStatisticAveragesCardProps) => {
+  return <Paper sx={{ height: 252 }}>StatisticAveragesCard</Paper>;
+};
+
+export default StatisticAveragesCard;
