e01a054e79ec03776bd10f8299647be6268236c7
Brian Gao <bhgao20014@gmail.com>
2022-04-28 13:23:54 -0400

    feat(subs): implement made for kids custom message

--- src/features/Dashboard/Subscriptions/components/MadeForKidsCard.tsx
+++ src/features/Dashboard/Subscriptions/components/MadeForKidsCard.tsx
@@ -8,15 +8,29 @@ interface IMadeForKidsCardProps {
 }
 
 const MadeForKidsCard = ({ madeForKidsRatio }: IMadeForKidsCardProps) => {
+  const getCustomMessage = (ratio: [number, number]): string => {
+    const percentage = (ratio[0] * 100) / ratio[1];
+    if (percentage < 25) {
+      return "Not many!";
+    } else if (percentage < 50) {
+      return "A decent amount.";
+    } else if (percentage < 75) {
+      return "That's a lot!";
+    } else {
+      return "Holy cow!";
+    }
+  };
+
   return (
     <OverlayScrollbar>
       <Paper sx={{ height: 252, p: 3 }}>
         <Typography variant="h6">Made for Kids</Typography>
         <Typography variant="h1" color="primary">
-          {(madeForKidsRatio[0] / madeForKidsRatio[1]).toFixed()}%
+          {((madeForKidsRatio[0] * 100) / madeForKidsRatio[1]).toFixed()}%
         </Typography>
         <Typography variant="body1">
-          of your subscriptions were made for kids. Not many!
+          of your subscriptions were made for kids.{" "}
+          {getCustomMessage(madeForKidsRatio)}
         </Typography>
       </Paper>
     </OverlayScrollbar>
