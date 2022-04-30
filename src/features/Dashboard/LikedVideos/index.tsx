3f0df8f939075762dc1cb8f2029db585c43e2df4
Brian Gao <bhgao20014@gmail.com>
2022-04-29 20:42:48 -0400

    feat(likedVids): implement base skeleton

--- src/features/Dashboard/LikedVideos/index.tsx
+++ src/features/Dashboard/LikedVideos/index.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Video } from "@types";
+import { getLikedVideos } from "common/utils/apiUtils";
+
 const LikedVideos = () => {
-  return <div>Liked Videos</div>;
+  // Gets liked videos on load
+  useEffect(() => {
+    let abortController = new AbortController();
+    getLikedVideos(100).then((likedVideos) => processLikedVideos(likedVideos));
+    return () => abortController.abort();
+  }, []);
+
+  const processLikedVideos = (videos: Video[] | undefined): void => {
+    if (videos === undefined) {
+      return;
+    }
+    console.log(videos);
+  };
+  return (
+    <Box>
+      <Typography>Liked Videos</Typography>
+    </Box>
+  );
 };
 
 export default LikedVideos;
