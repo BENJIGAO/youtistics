4263f5bfa04ba71d8866b499de7ee4b68bf6f8d6
Brian Gao <bhgao20014@gmail.com>
2022-11-02 20:18:37 -0400

    feat(app): created logo

--- src/common/components/YoutisticsLogo.tsx
+++ src/common/components/YoutisticsLogo.tsx
@@ -1,18 +1,18 @@
-import Toolbar from "@mui/material/Toolbar";
 import BubbleChartIcon from "@mui/icons-material/BubbleChart";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Typography from "@mui/material/Typography";
 
 const YoutisticsLogo = () => {
   return (
-    <Toolbar
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        px: 1,
-      }}
-    >
-      Youtistics
-    </Toolbar>
+    <ListItem>
+      <ListItemIcon>
+        <BubbleChartIcon color="error" fontSize="large" />
+      </ListItemIcon>
+      <Typography variant="button" fontSize={18}>
+        Youtistics
+      </Typography>
+    </ListItem>
   );
 };
 
